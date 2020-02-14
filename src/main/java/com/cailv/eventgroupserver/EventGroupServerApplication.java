b8301f93c42785d9eeb374105199b2fdab0cf68f
AL-001 <2695605447@qq.com>
2020-02-14 16:01:46 +0800

    init

---
+++ src/main/java/com/cailv/eventgroupserver/EventGroupServerApplication.java
@@ -0,0 +1,13 @@
+package com.cailv.eventgroupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGroupServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventGroupServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cailv/eventgroupserver/EventGroupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cailv.eventgroupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGroupServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
